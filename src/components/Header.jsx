5c72ea0f76f6c458a4eb5a5053b43acdad52b1dc
Jettechnologies <tomesitime04@gmail.com>
2024-07-12 14:51:23 +0000

    Fixed the cta button focus issues

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -181,7 +181,7 @@ const Header = () => {
           <Box>
             <Heading fontSize={{ base: "3xl", md: "4xl", lg: "5xl" }} mb={4}>Unleash the power of AI Trading</Heading>
             <Text fontSize={{ base: "md", md: "lg", lg: "xl" }} mb={8} >THE FOREX IQ- YOUR TRUSTED PARTNER IN AUTOMATED TRADING</Text>
-            <Button px={{ md: "4rem" }} bg="#011b3d" color="white" size="lg" width={{ base: "100%", md: "auto" }}>Start Trading Now</Button>
+            <Button px={{ md: "4rem" }} bg="#011b3d" color="white" _focus={{ color: "black" }} size="lg" width={{ base: "100%", md: "auto" }}>Start Trading Now</Button>
           </Box>
         </Flex>
       </Box>
